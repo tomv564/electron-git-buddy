831cb90b51c2f7ef69db5593c8229b74383776e1
Tom van Ommeren <tom.vanommeren@gmail.com>
2015-12-06 18:14:04 +0100

    Packaging works with the exception of font loading (publicPath?)

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -17,17 +17,22 @@ config.output.publicPath = '/dist/';
 
 config.module.loaders.push({
   test: /^((?!\.module).)*\.css$/,
-  loader: ExtractTextPlugin.extract(
+  loaders: [
     'style-loader',
     'css-loader'
-  )
+  ]
 }, {
   test: /\.module\.css$/,
   loader: ExtractTextPlugin.extract(
     'style-loader',
     'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]'
   )
-});
+},
+  {test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff'},
+  {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
+  {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
+  {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
+);
 
 config.plugins.push(
   new webpack.optimize.OccurenceOrderPlugin(),
